e270fe2daaf5ba032f292a1d48e43436bdbe2942
YevheniiVish <55807961+YevheniiVish@users.noreply.github.com>
2020-05-28 05:03:38 +0300

    add

---
+++ goit-js-hw-12-countries/main.5671fc00578a85d63d5f.js
@@ -0,0 +1,301 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    "/nVi": function (n, e, t) {},
+    "5aCR": function (n, e, t) {},
+    "Lq6+": function (n, e, t) {
+      var l = t("mp5j");
+      n.exports = (l.default || l).template({
+        1: function (n, e, t, l, a) {
+          var o,
+            r,
+            u = null != e ? e : n.nullContext || {},
+            c = n.hooks.helperMissing,
+            i = "function",
+            s = n.escapeExpression,
+            p =
+              n.lookupProperty ||
+              function (n, e) {
+                if (Object.prototype.hasOwnProperty.call(n, e)) return n[e];
+              };
+          return (
+            '<li class="country">\r\n    <div class="country-container ">\r\n        <p class="country-name">' +
+            s(
+              typeof (r =
+                null != (r = p(t, "name") || (null != e ? p(e, "name") : e))
+                  ? r
+                  : c) === i
+                ? r.call(u, {
+                    name: "name",
+                    hash: {},
+                    data: a,
+                    loc: {
+                      start: { line: 4, column: 32 },
+                      end: { line: 4, column: 40 },
+                    },
+                  })
+                : r
+            ) +
+            '</p>\r\n        <p class="country-capital">Capital:  ' +
+            s(
+              typeof (r =
+                null !=
+                (r = p(t, "capital") || (null != e ? p(e, "capital") : e))
+                  ? r
+                  : c) === i
+                ? r.call(u, {
+                    name: "capital",
+                    hash: {},
+                    data: a,
+                    loc: {
+                      start: { line: 5, column: 45 },
+                      end: { line: 5, column: 56 },
+                    },
+                  })
+                : r
+            ) +
+            '</p>\r\n        <p class="country-population">Population:  ' +
+            s(
+              typeof (r =
+                null !=
+                (r = p(t, "population") || (null != e ? p(e, "population") : e))
+                  ? r
+                  : c) === i
+                ? r.call(u, {
+                    name: "population",
+                    hash: {},
+                    data: a,
+                    loc: {
+                      start: { line: 6, column: 51 },
+                      end: { line: 6, column: 65 },
+                    },
+                  })
+                : r
+            ) +
+            '</p>\r\n\r\n        <ul class="country-languages">\r\n            languages: ' +
+            (null !=
+            (o = p(t, "each").call(u, null != e ? p(e, "languages") : e, {
+              name: "each",
+              hash: {},
+              fn: n.program(2, a, 0),
+              inverse: n.noop,
+              data: a,
+              loc: {
+                start: { line: 9, column: 23 },
+                end: { line: 11, column: 21 },
+              },
+            }))
+              ? o
+              : "") +
+            '        </ul>\r\n\r\n    </div>\r\n    <img src="' +
+            s(
+              typeof (r =
+                null != (r = p(t, "flag") || (null != e ? p(e, "flag") : e))
+                  ? r
+                  : c) === i
+                ? r.call(u, {
+                    name: "flag",
+                    hash: {},
+                    data: a,
+                    loc: {
+                      start: { line: 15, column: 14 },
+                      end: { line: 15, column: 22 },
+                    },
+                  })
+                : r
+            ) +
+            '" alt="' +
+            s(
+              typeof (r =
+                null != (r = p(t, "name") || (null != e ? p(e, "name") : e))
+                  ? r
+                  : c) === i
+                ? r.call(u, {
+                    name: "name",
+                    hash: {},
+                    data: a,
+                    loc: {
+                      start: { line: 15, column: 29 },
+                      end: { line: 15, column: 37 },
+                    },
+                  })
+                : r
+            ) +
+            '" width="300" height="200">\r\n</li>\r\n'
+          );
+        },
+        2: function (n, e, t, l, a) {
+          var o =
+            n.lookupProperty ||
+            function (n, e) {
+              if (Object.prototype.hasOwnProperty.call(n, e)) return n[e];
+            };
+          return (
+            '\r\n            <li class="country-languages__item">' +
+            n.escapeExpression(n.lambda(null != e ? o(e, "name") : e, e)) +
+            "</li>\r\n"
+          );
+        },
+        compiler: [8, ">= 4.3.0"],
+        main: function (n, e, t, l, a) {
+          var o;
+          return null !=
+            (o = (
+              n.lookupProperty ||
+              function (n, e) {
+                if (Object.prototype.hasOwnProperty.call(n, e)) return n[e];
+              }
+            )(t, "each").call(null != e ? e : n.nullContext || {}, e, {
+              name: "each",
+              hash: {},
+              fn: n.program(1, a, 0),
+              inverse: n.noop,
+              data: a,
+              loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 17, column: 9 },
+              },
+            }))
+            ? o
+            : "";
+        },
+        useData: !0,
+      });
+    },
+    QfWi: function (n, e, t) {
+      "use strict";
+      t.r(e);
+      t("5aCR"), t("/nVi");
+      var l = t("dIfx"),
+        a = (t("UOjr"), t("jffb")),
+        o = t.n(a),
+        r =
+          (t("JBxO"),
+          t("FdtR"),
+          function (n) {
+            return fetch("https://restcountries.eu/rest/v2/name/" + n)
+              .then(function (n) {
+                return n.json();
+              })
+              .catch(function (n) {
+                return console.warn(n);
+              });
+          }),
+        u = t("lbwj"),
+        c = t.n(u),
+        i = t("Lq6+"),
+        s = t.n(i),
+        p = {
+          searchInput: document.querySelector("#input"),
+          countryList: document.querySelector('ul[data-action="countries"]'),
+        };
+      p.searchInput.addEventListener(
+        "input",
+        o()(function (n) {
+          if (" " === n.target.value || "" === n.target.value)
+            return (
+              l.a.notice({
+                text: "Enter a country name?",
+                type: "notice",
+                delay: 2e3,
+              }),
+              void e()
+            );
+          function e() {
+            var n = document.querySelector(".ui-pnotify");
+            n.addEventListener("click", function (e) {
+              e.target && n.remove();
+            });
+          }
+          r(n.target.value)
+            .then(function (n) {
+              return 1 === n.length
+                ? ((p.countryList.innerHTML = "" + s()(n)),
+                  l.a.success({
+                    title: "Your country!",
+                    type: "success",
+                    text: "This is your country?",
+                    delay: 2e3,
+                  }),
+                  void e())
+                : (n.length >= 2 &&
+                    n.length <= 10 &&
+                    (p.countryList.innerHTML = "" + c()(n)),
+                  data.length > 10
+                    ? (l.a.error({
+                        text:
+                          "Too many matches found. Please enter a more specific query!",
+                        type: "error",
+                        delay: 2e3,
+                      }),
+                      void e())
+                    : void 0);
+            })
+            .catch(function (n) {
+              return console.warn(n);
+            });
+        }, 500)
+      );
+    },
+    lbwj: function (n, e, t) {
+      var l = t("mp5j");
+      n.exports = (l.default || l).template({
+        1: function (n, e, t, l, a) {
+          var o,
+            r =
+              n.lookupProperty ||
+              function (n, e) {
+                if (Object.prototype.hasOwnProperty.call(n, e)) return n[e];
+              };
+          return (
+            '<li class="country country__name">' +
+            n.escapeExpression(
+              "function" ==
+                typeof (o =
+                  null != (o = r(t, "name") || (null != e ? r(e, "name") : e))
+                    ? o
+                    : n.hooks.helperMissing)
+                ? o.call(null != e ? e : n.nullContext || {}, {
+                    name: "name",
+                    hash: {},
+                    data: a,
+                    loc: {
+                      start: { line: 2, column: 34 },
+                      end: { line: 2, column: 42 },
+                    },
+                  })
+                : o
+            ) +
+            "</li>\r\n"
+          );
+        },
+        compiler: [8, ">= 4.3.0"],
+        main: function (n, e, t, l, a) {
+          var o;
+          return null !=
+            (o = (
+              n.lookupProperty ||
+              function (n, e) {
+                if (Object.prototype.hasOwnProperty.call(n, e)) return n[e];
+              }
+            )(t, "each").call(null != e ? e : n.nullContext || {}, e, {
+              name: "each",
+              hash: {},
+              fn: n.program(1, a, 0),
+              inverse: n.noop,
+              data: a,
+              loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 3, column: 9 },
+              },
+            }))
+            ? o
+            : "";
+        },
+        useData: !0,
+      });
+    },
+  },
+  [["QfWi", 1, 2]],
+]);
+//# sourceMappingURL=main.5671fc00578a85d63d5f.js.map
